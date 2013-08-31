92f9f6b71ac763510c92e367483d7c570b59a70c
Nishimura Makoto <mac.nishimura@gmail.com>
2013-08-31 23:39:03 +0900

    書籍を見ながらコードを追加・・・だけど、書籍のコードがストーリーボード使わないタイプだった・・・

--- VarEx/macViewController.m
+++ VarEx/macViewController.m
@@ -18,6 +18,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    int num0;
+    int num1;
+    int sum;
+    
+    num0 = 100;
+    num1 = 200;
+    sum = num0 + num1;
+    
+    NSString* text = [NSString stringWithFormat:@"合計=%d", sum];
+    [[UIColor blackColor] set];
+    UIFont* font =  [UIFont systemFontOfSize:24];
+    [text drawAtPoint:CGPointMake(0, 0) withFont:font];
 }
 
 - (void)didReceiveMemoryWarning
